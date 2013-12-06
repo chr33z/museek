7a4157828c69c8f7fbe272cda8514e1822910347
chr33z <chr33z@googlemail.com>
2013-12-06 12:57:04 +0100

    Button auf Startscreen eingebaut zu Graph Test

--- src/de/mimuc/pem_music_graph/StartScreen.java
+++ src/de/mimuc/pem_music_graph/StartScreen.java
@@ -1,15 +1,36 @@
 package de.mimuc.pem_music_graph;
 
+import de.mimuc.pem_music_graph.graph.GraphTest;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class StartScreen extends Activity {
 
+	Button btnGraph;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_startscreen);
+		
+		btnGraph = (Button) findViewById(R.id.btn_graph);
+		
+		btnGraph.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				switch(v.getId()){
+				case R.id.btn_graph:
+					startActivity(new Intent(getApplicationContext(), GraphTest.class));
+					break;
+				}
+			}
+		});
 	}
 
 	@Override
